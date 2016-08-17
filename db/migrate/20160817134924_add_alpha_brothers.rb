cdf15490d5ee6ba3b1a94697afb0a39f47bc091d
DrewHiggins <me@drewhiggins.com>
2016-08-17 09:57:11 -0400

    added alpha family migration

---
+++ db/migrate/20160817134924_add_alpha_brothers.rb
@@ -0,0 +1,16 @@
+class AddAlphaBrothers < ActiveRecord::Migration
+  def up
+    alpha_fam = Family.find_or_create_by(name: 'Alpha')
+    rc = RushClass.find_or_create_by(semester: 'Fall', year: 1992, greek_name: 'Beta')
+    bro0 = Brother.create(first_name: 'Jodi', last_name: 'Angel', status: 'Alumni', family_id: alpha_fam.id)
+    bro1 = Brother.create(first_name: 'Heather', last_name: 'Salko', status: 'Alumni', rush_class_id: rc.id, parent_id: bro0.id, family_id: alpha_fam.id)
+  end
+
+  def down
+    alpha_fam = Family.find_by(name: 'Alpha')
+    Brother.where(family_id: alpha_fam.id).each do |brother|
+      brother.destroy
+    end
+    alpha_fam.destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817132000) do
+ActiveRecord::Schema.define(version: 20160817134924) do
 
   create_table "branches", force: :cascade do |t|
     t.string   "name"
