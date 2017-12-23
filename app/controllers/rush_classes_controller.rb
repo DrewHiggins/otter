4fb2a58fec1c0d665570e95ba3c59426f1008f16
Drew Higgins <me@drewhiggins.com>
2017-12-23 16:23:55 -0500

    Added rush class CRUD
    Also added the transferred brother state, and fixed a bug with deleting brothers.

--- app/controllers/rush_classes_controller.rb
+++ app/controllers/rush_classes_controller.rb
@@ -1,2 +1,50 @@
 class RushClassesController < ApplicationController
+  def index
+    @rush_classes = RushClass.all.order(:year)
+  end
+
+  def show
+    @rush_class = RushClass.find(params[:id])
+    @class_members = Brother.where({ rush_class_id: params[:id] })
+  end
+
+  def new
+    @rush_class = RushClass.new
+  end
+
+  def create
+    @rush_class = RushClass.create(rush_class_params)
+    if @rush_class.save
+      redirect_to @rush_class, notice: "Successfully created rush class!"
+    end
+  end
+
+  def edit
+    @rush_class = RushClass.find(params[:id])
+  end
+
+  def update
+    @rush_class = RushClass.find(params[:id])
+
+    if @rush_class.update_attributes(rush_class_params)
+      redirect_to :action => "show", id: @rush_class.id
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    if RushClass.destroy(params[:id])
+      redirect_to '/rush_classes', notice: "Rush class successfully destroyed"
+    else
+      redirect_to @rush_class, notice: "Error: Could not delete rush class"
+    end
+  end
+
+  private
+  def rush_class_params
+    params.require(:rush_class).permit(
+      :greek_name, :year, :semester
+    )
+  end
 end
